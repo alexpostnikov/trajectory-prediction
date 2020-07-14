a3f01db960737c2925479bf773735392d17d56b6
alex.postnikov <alex.postnikov@skolkovotech.ru>
2020-07-14 14:18:51 +0300

    first model

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
 torch.manual_seed(1)
+from torch.distributions.multivariate_normal import MultivariateNormal
 
 input_dim = 2 # x, y
 hidden_dim = 10
@@ -400,6 +401,196 @@ class LstmEncDeltaStacked(nn.Module):
         tag_space = (scene.clone()[:, -1, :] + self.hidden2pose(dec_reshaped)).unsqueeze(1)
         return tag_space
 
+
+
+
+
+class LstmEncDeltaStackedVel(nn.Module):
+    """
+    model that actually predicts delta movements (output last timestamp + predicted delta),
+    with encoding person history and neighbors relative positions.
+    """
+
+    def __init__(self, lstm_hidden_dim, target_size, num_layers=1, embedding_dim=10, bidir=True):
+        super(LstmEncDeltaStackedVel, self).__init__()
+        self.name = "LstmEncDeltaStackedVel"
+        self.embedding_dim = embedding_dim
+        self.lstm_hidden_dim = lstm_hidden_dim
+        self.num_layers = num_layers
+        self.dir_number = 1
+        if bidir:
+            self.dir_number = 2
+        self.node_hist_encoder = nn.LSTM(input_size=4,
+                                           hidden_size=lstm_hidden_dim,
+                                           num_layers=num_layers,
+                                           bidirectional=bidir,
+                                           batch_first=True,
+                                           dropout=0.5)
+
+        self.node_vel_hist_encoder = nn.LSTM(input_size=4,
+                                         hidden_size=lstm_hidden_dim,
+                                         num_layers=num_layers,
+                                         bidirectional=bidir,
+                                         batch_first=True,
+                                         dropout=0.5)
+
+        self.edge_encoder = nn.LSTM(input_size=4,
+                                  hidden_size=lstm_hidden_dim,
+                                  num_layers=num_layers,
+                                  bidirectional=bidir,
+                                  batch_first=True,
+                                  dropout=0.5)
+
+        self.decoder = nn.LSTM(input_size=self.dir_number * lstm_hidden_dim,
+                                    hidden_size=embedding_dim,
+                                    num_layers=num_layers,
+                                    bidirectional=bidir,
+                                    batch_first=True,
+                                    dropout=0.5)
+
+        # The linear layer that maps from hidden state space to pose space
+
+        self.hidden2pose = nn.Linear(9 * self.dir_number * embedding_dim, target_size)
+
+    def forward(self, scene: torch.Tensor) -> torch.Tensor:
+        """
+        :param scene: tensor of shape num_peds, history_size, data_dim
+        :return: predicted poses for each agent at next timestep
+        """
+        bs = scene.shape[0]
+        inp = scene
+
+        lstm_out, hid = self.node_hist_encoder(inp)  # lstm_out shape num_peds, timestamps ,  2*hidden_dim
+        # lstm_out_vel, _ = self.node_vel_hist_encoder(inp[..., 2:4])
+        current = scene[:, -1, :]# num_people, data_dim
+        np, data_dim = current.shape
+        stacked = current.flatten().repeat(np).reshape(np, np * data_dim)
+        deltas = (stacked - current.repeat(1, np)).reshape(np, np, data_dim)  # np, np, data_dim
+        distruction, _ = self.edge_encoder(deltas)
+        # catted = torch.cat((lstm_out+lstm_out_vel, distruction[:, -1:, :]), dim=1)
+        catted = torch.cat((lstm_out, distruction[:, -1:, :]), dim=1)
+        decoded, _ = self.decoder(catted)
+        dec_reshaped = decoded.reshape(bs, -1)
+        tag_space = (scene.clone()[:, -1, :2] + self.hidden2pose(dec_reshaped)).unsqueeze(1)
+        return tag_space
+
+
+class LstmEncDeltaStackedFullPred(nn.Module):
+    """
+    model that actually predicts delta movements (output last timestamp + predicted delta),
+    with encoding person history and neighbors relative positions.
+    """
+
+    def __init__(self, lstm_hidden_dim, target_size, num_layers=1, embedding_dim=10, bidir=True):
+        super(LstmEncDeltaStackedFullPred, self).__init__()
+        self.name = "LstmEncDeltaStackedFullPred"
+        self.embedding_dim = embedding_dim
+        self.lstm_hidden_dim = lstm_hidden_dim
+        self.num_layers = num_layers
+        self.dir_number = 1
+        if bidir:
+            self.dir_number = 2
+        self.node_hist_encoder = nn.LSTM(input_size=4,
+                                         hidden_size=lstm_hidden_dim,
+                                         num_layers=num_layers,
+                                         bidirectional=bidir,
+                                         batch_first=True,
+                                         dropout=0.5)
+
+        self.edge_encoder = nn.LSTM(input_size=2,
+                                    hidden_size=lstm_hidden_dim,
+                                    num_layers=num_layers,
+                                    bidirectional=bidir,
+                                    batch_first=True,
+                                    dropout=0.5)
+
+        self.decoder = nn.LSTM(input_size=self.dir_number * lstm_hidden_dim,
+                               hidden_size=embedding_dim,
+                               num_layers=num_layers,
+                               bidirectional=bidir,
+                               batch_first=True,
+                               dropout=0.5)
+
+        self.gru = nn.GRUCell(182, 128)
+
+        self.action = nn.Linear(4, 2)
+        self.state = nn.Linear(180, 128)
+
+        # The linear layer that maps from hidden state space to pose space
+
+        self.hidden2pose = nn.Linear(9 * self.dir_number * embedding_dim, target_size)
+        self.proj_to_GMM_log_pis = nn.Linear(128, 1)
+        self.proj_to_GMM_mus = nn.Linear(128, 2)
+        self.proj_to_GMM_log_sigmas = nn.Linear(128, 2)
+        self.proj_to_GMM_corrs = nn.Linear(128, 1)
+
+    def project_to_GMM_params(self, tensor) -> (torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor):
+        """
+        Projects tensor to parameters of a GMM with N components and D dimensions.
+
+        :param tensor: Input tensor.
+        :return: tuple(log_pis, mus, log_sigmas, corrs)
+            WHERE
+            - log_pis: Weight (logarithm) of each GMM component. [N]
+            - mus: Mean of each GMM component. [N, D]
+            - log_sigmas: Standard Deviation (logarithm) of each GMM component. [N, D]
+            - corrs: Correlation between the GMM components. [N]
+        """
+        log_pis = self.proj_to_GMM_log_pis(tensor)
+        mus = self.proj_to_GMM_mus(tensor)
+        log_sigmas = self.proj_to_GMM_log_sigmas(tensor)
+        corrs = torch.tanh(self.proj_to_GMM_corrs(tensor))
+        return log_pis, mus, log_sigmas, corrs
+
+    def forward(self, scene: torch.Tensor):
+        """
+        :param scene: tensor of shape num_peds, history_size, data_dim
+        :return: predicted poses for each agent at next timestep
+        """
+        bs = scene.shape[0]
+        inp = scene
+
+        lstm_out, hid = self.node_hist_encoder(inp)  # lstm_out shape num_peds, timestamps ,  2*hidden_dim
+
+
+        current_pose = scene[:, -1, :2]  # num_people, data_dim
+        current_state = scene[:, -1, :]
+        np, data_dim = current_pose.shape
+        stacked = current_pose.flatten().repeat(np).reshape(np, np * data_dim)
+        deltas = (stacked - current_pose.repeat(1, np)).reshape(np, np, data_dim)  # np, np, data_dim
+
+        distruction, _ = self.edge_encoder(deltas)
+
+
+        # catted = torch.cat((lstm_out+lstm_out_vel, distruction[:, -1:, :]), dim=1)
+        catted = torch.cat((lstm_out, distruction[:, -1:, :]), dim=1)
+
+        a_0 = self.action(current_state.reshape(bs, -1))
+
+        state = self.state(catted.reshape(bs, -1))
+
+        gauses = []
+        inp = torch.cat((catted.reshape(bs, -1), a_0), dim=-1)
+        for i in range(12):
+
+            h_state = self.gru(inp.reshape(bs, -1), state)
+
+            log_pis, mus, log_sigmas, corrs = self.project_to_GMM_params(h_state)
+
+            variance = torch.exp(log_sigmas).unsqueeze(1) ** 2
+            m_diag = variance * torch.eye(2)
+            sigma_xy = torch.prod(torch.exp(log_sigmas), dim=-1)
+            t = (sigma_xy * corrs.squeeze()).reshape(-1, 1, 1)
+            anti_diag = t * torch.tensor([[0, 1], [1, 0]])
+            cov_matrix = m_diag + anti_diag
+            gaus = MultivariateNormal(mus, cov_matrix)
+            gauses.append(gaus)
+            a_t = gaus.rsample()
+            state = h_state
+            inp = torch.cat((catted.reshape(bs, -1), a_t), dim=-1)
+
+        return gauses
+
 class OneLayer(nn.Module):
     def __init__(self):
         super(OneLayer, self).__init__()
@@ -414,6 +605,8 @@ class OneLayer(nn.Module):
         out = self.simple_linear(self_pose)
 
         return out.reshape(1, 1, 2)
+
+
 if __name__ == "__main__":
     # model = LSTM_hid(10, 2, 2)
     model = LSTM_delta(10, 20, 2)
@@ -425,6 +618,6 @@ if __name__ == "__main__":
         inp = torch.cat((self_pose[:, :, 0+timestamp:8+timestamp, :], others_pose[:, :, 0+timestamp:8+timestamp, :]), dim=1)[0, :, :, :]
         print(inp.shape)
         prediction = model(inp)
-        print (prediction.shape)
+        print(prediction.shape)
         predictions = torch.cat((predictions, prediction.unsqueeze(0)), dim=2)
     print(predictions.shape)
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import os
 import torch
 import torch.optim as optim
 from dataloader import Dataset_from_pkl, is_filled
-from model import LstmEncDeltaStacked
+from model import LstmEncDeltaStacked, LstmEncDeltaStackedVel, LstmEncDeltaStackedFullPred
 from torch.utils.tensorboard import SummaryWriter
 
 from utils import compare_prediction_gt
@@ -66,6 +66,48 @@ def get_ade_fde(generator: torch.utils.data.DataLoader, model: torch.nn.Module, 
     fde = sum(fde) / len(fde)
     return (ade, fde)
 
+
+
+def get_ade_fde_vel(generator: torch.utils.data.DataLoader, model: torch.nn.Module, limit: int = 1e10) -> Tuple[int, int]:
+    """
+
+    :param generator: torch generator to get data
+    :param model:   torch module predicting poses. input shape are [numped, history, 2]
+    :param limit: limit number of processed batches. Default - unlimited
+    :return: tuple of ade, fde for given generator and limit of batches
+    """
+    ade = []
+    fde = []
+    for batch_id, local_batch in enumerate(generator):
+
+        if batch_id > limit:
+            break
+        if local_batch.shape[1] < 1:
+            continue
+        gt = local_batch.clone()
+        local_batch = local_batch.to(device)
+        local_batch[0, :, 8:, :] = torch.zeros_like(local_batch[0, :, 8:, :])
+        num_peds = local_batch.shape[1]
+        predictions = torch.zeros(num_peds, 0, 2).requires_grad_(True).to(device)
+
+        for t in range(0, 12):
+            prediction = model(local_batch[0, :, 0 + t: 8 + t, 2:6])
+
+            #                 prediction = prediction.unsqueeze(1)
+            predictions = torch.cat((predictions, prediction), dim=1)
+            new_vel = (prediction - local_batch[0, :, 7 + t:8 + t, :2]) / 0.4
+            # local_batch = torch.cat((local_batch, torch.cat((prediction, new_vel), dim=2).unsqueeze(0)), dim=2)
+            local_batch[0, :, 8 + t:9 + t, 2:6] = torch.cat((prediction, new_vel), dim=2).detach()
+
+        metrics = compare_prediction_gt(predictions.detach().cpu(), gt.detach().cpu()[0][:, :, 2:4])
+
+        for local_ade in metrics["ade"]:
+            ade.append(local_ade)
+        for local_fde in metrics["fde"]:
+            fde.append(local_fde)
+    ade = sum(ade) / len(ade)
+    fde = sum(fde) / len(fde)
+    return (ade, fde)
 
 def get_batch_is_filled_mask(batch: torch.Tensor) -> Tuple[torch.Tensor, int]:
     """
@@ -174,6 +216,97 @@ def train(model: torch.nn.Module, training_generator: torch.utils.data.DataLoade
     torch.save(model.state_dict(), save_model_path)
 
 
+def train_pose_vel(model: torch.nn.Module, training_generator: torch.utils.data.DataLoader,
+          test_generator: torch.utils.data.DataLoader, num_epochs:int, device: torch.device,
+          lr: Union[int, float] = 0.02, limit: Union[int, float] = 10e7):
+    """
+
+    :param model: model for predicting the poses.  input shape are [numped, history, 2]
+    :param training_generator:  torch training generator to get data
+    :param test_generator: torch training generator to get data
+    :param num_epochs: number of epochs to train
+    :param device: torch device. (cpu/cuda)
+    :param lr: learning rate
+    :param limit: limit the number of training batches
+    :return:
+    """
+
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    drop_every_epochs = 10
+    drop_rate = 1
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, drop_every_epochs,
+                                                drop_rate)  # drop_every_epochs epochs drop by drop_rate lr
+    writer, experiment_name, save_model_path = setup_experiment(model.name+str(lr) + "_hd_" + str(model.lstm_hidden_dim)
+                                                                +"_ed_"  + str(model.embedding_dim)
+                                                                + "_nl_"+str(model.num_layers))
+    prev_epoch_loss = 0
+
+    for epoch in range(0, num_epochs):
+        model.train()
+        epoch_loss = 0
+        start = time.time()
+        num_skipped = 0
+        for batch_id, local_batch in enumerate(training_generator):
+            if batch_id > limit:
+                # skip to next epoch
+                continue
+
+            # angle = torch.rand(1) * math.pi
+            # rot = torch.tensor([[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]])
+            # local_batch[:, :, :, 2:6] = local_batch[:, :, :, 2:6] @ rot
+            local_batch = local_batch.to(device)
+            gt = local_batch.clone()
+            model.zero_grad()
+            num_peds = local_batch.shape[1]
+            predictions = torch.zeros(num_peds, 0, 2).requires_grad_(True).to(device)
+            mask, full_peds = get_batch_is_filled_mask(gt)
+            if full_peds == 0:
+                continue
+            mask = mask.to(device)
+
+            local_batch = local_batch[:, :, :8, 2:6]
+            if torch.sum(mask) == 0.0:
+                num_skipped += 1
+                continue
+            for t in range(0, 12):
+                prediction = model(local_batch[0, :, 0 + t:8 + t, :])
+                predictions = torch.cat((predictions, prediction), dim=1)
+                new_vel = gt[0, :, 8+t:9+t:, 4:6]
+                local_batch = torch.cat((local_batch, torch.cat((prediction, new_vel), dim=2).unsqueeze(0)), dim=2)
+
+            loss = torch.sum(torch.norm(mask * (predictions - gt[0, :, 8:, 2:4]), dim=-1))
+            epoch_loss += loss.item() / full_peds
+            loss.backward()
+            optimizer.step()
+
+        epoch_loss = epoch_loss / (batch_id - num_skipped)
+        print("epoch {epoch} loss {el:0.4f}, time taken {t:0.2f}, delta {delta:0.3f}".format(epoch=epoch, el=epoch_loss,
+                                                                                             t=time.time() - start,
+                                                                                             delta=-prev_epoch_loss + epoch_loss))
+
+        if writer is not None:
+            writer.add_scalar(f"loss_epoch", epoch_loss, epoch)
+
+        prev_epoch_loss = epoch_loss
+        model.eval()
+        start = time.time()
+        with torch.no_grad():
+            ade, fde = get_ade_fde_vel(test_generator, model)
+
+            if writer is not None:
+                writer.add_scalar(f"test/ade", ade, epoch)
+                writer.add_scalar(f"test/fde", fde, epoch)
+            t_ade, t_fde = get_ade_fde_vel(training_generator, model, 100)
+            if writer is not None:
+                writer.add_scalar(f"train/ade", t_ade, epoch)
+                writer.add_scalar(f"train/fde", t_fde, epoch)
+        print("\t epoch {epoch} val ade {ade:0.4f}, val fde {fde:0.4f} time taken {t:0.2f}".format(epoch=epoch, ade=ade,
+                                                                                                   t=time.time()-start,
+                                                                                                   fde=fde))
+    torch.save(model.state_dict(), save_model_path)
+
+
+
 if __name__ == "__main__":
     training_set = Dataset_from_pkl("/home/robot/repos/trajectory-prediction/processed/", data_files=["eth_train.pkl"])
     training_generator = torch.utils.data.DataLoader(training_set, batch_size=1, shuffle=True)
@@ -189,7 +322,9 @@ if __name__ == "__main__":
 
     # model = LSTM_delta(40, 20, 2, 1).to(device)
 
-    model = LstmEncDeltaStacked(lstm_hidden_dim=10, target_size=2, num_layers=1, embedding_dim=10, bidir=False).to(device)
+    # model = LstmEncDeltaStacked(lstm_hidden_dim=10, target_size=2, num_layers=1, embedding_dim=10, bidir=False).to(device)
+    model = LstmEncDeltaStackedVel(lstm_hidden_dim=30, target_size=2, num_layers=1, embedding_dim=10, bidir=False).to(device)
+
     # model = LSTM_enc_delta_wo_emb(10, 2, embedding_dim=10).to(device)
 
-    train(model, training_generator, test_generator, num_epochs=100, device=device, lr=0.002, limit=1e100)
+    train_pose_vel(model, training_generator, test_generator, num_epochs=100, device=device, lr=0.002, limit=200)
